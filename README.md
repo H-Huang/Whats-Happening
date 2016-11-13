e2660a3931337eb65572745f95965ab585e84db3
Howard Huang <howardhuang96@gmail.com>
2016-11-12 17:22:31 -0800

    server provisioning shell scripts + vagrant

---
+++ README.md
@@ -0,0 +1,12 @@
+CS M117 Android Application
+
+The project contains two parts:
+-the android application
+-Django RESTful api as the backend
+
+Steps to develop on it:
+
+1. download vagrant, android studio, and virtual box if you dont have it
+2. cd into backend-code folder and run the command "vagrant up" (this will create a virtual machine for your backend server)
+3. ssh into your virtual machine on 127.0.0.1:2222, with user: vagrant (no password)
+4. now you can work make changes to the django backend or the android application
---
+++ backend-code/serverProvision/initial.sh
@@ -0,0 +1,54 @@
+echo $1 #projectName
+echo $2 #projectUser
+echo $3 #password
+
+sudo apt-get update -y
+
+# git
+sudo apt-get install git -y
+
+# Python
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3.4-dev -y
+
+# Nginx setup
+sudo apt-get install nginx -y
+sudo mv $1 /etc/nginx/sites-available/$1
+sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled
+sudo service nginx restart
+
+#supervisor setup
+sudo apt-get install supervisor -y
+sudo cp gunicorn.conf /etc/supervisor/conf.d/gunicorn.conf
+sudo service supervisor restart
+
+#postgresql
+sudo apt-get install libpq-dev -y
+sudo apt-get install postgresql -y
+sudo apt-get install postgresql-contrib -y
+
+#create the database
+sudo -u postgres -i psql -c "CREATE DATABASE $1;"
+sudo -u postgres -i psql -c "CREATE USER $2 WITH PASSWORD '$3';"
+sudo -u postgres -i psql -c "GRANT ALL PRIVILEGES ON DATABASE $1 TO $2;"
+sudo -u postgres -i psql -c "ALTER ROLE $2 SET client_encoding TO 'utf8';"
+sudo -u postgres -i psql -c "ALTER ROLE $2 SET default_transaction_isolation TO 'read committed';"
+sudo -u postgres -i psql -c "ALTER ROLE $2 SET timezone TO 'UTC';"
+
+#upgrade pip
+sudo pip3 install --upgrade pip
+
+#creating virtual env
+sudo pip3 install virtualenv
+cd ~
+virtualenv $1
+source ~/$1/bin/activate
+
+#django, gunicorn, psycopg2
+pip install django
+pip install gunicorn
+pip install psycopg2
+
+#project specific packages
+pip install djangorestframework
