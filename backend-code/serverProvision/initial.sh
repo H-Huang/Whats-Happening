508a63c5bb9eae464156f1230d18353ac9483d97
Howard Huang <howardhuang96@gmail.com>
2016-11-19 13:44:29 -0800

    server setup script update

--- backend-code/serverProvision/initial.sh
+++ backend-code/serverProvision/initial.sh
@@ -14,13 +14,13 @@ sudo apt-get install python3.4-dev -y
 
 # Nginx setup
 sudo apt-get install nginx -y
-sudo mv serverProvision/$1 /etc/nginx/sites-available/$1
+sudo mv ./$1 /etc/nginx/sites-available/$1
 sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled
 sudo service nginx restart
 
 #supervisor setup
 sudo apt-get install supervisor -y
-sudo cp serverProvision/gunicorn.conf /etc/supervisor/conf.d/gunicorn.conf
+sudo cp ./gunicorn.conf /etc/supervisor/conf.d/gunicorn.conf
 sudo service supervisor restart
 
 #postgresql
